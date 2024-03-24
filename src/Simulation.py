3896825a78d5a4bc0408590b3f503f5348e62ce4
JamesMeta <jamesmata556@gmail.com>
2024-03-24 15:33:45 -0400

    Implemented Debug mode and the first test for drawing objects

--- src/Simulation.py
+++ src/Simulation.py
@@ -4,11 +4,18 @@ from organism import Organism
 from organism.animal import Animal
 from organism.animal.carnivor import Carnivor
 from organism.animal.herbivor import Herbivor
-from organism.animal.carnivor.carnivors import Fox, Owl, SmallBird, Hawk, Snake, Frog
-from organism.animal.herbivor.herbivors import Rabbit, GrassHopper, Mouse 
-from maplogic import World
-from maplogic import Resource
-from maplogic import Grass
+from organism.animal.carnivor.carnivors.Fox import Fox
+from organism.animal.carnivor.carnivors.Owl import Owl
+from organism.animal.carnivor.carnivors.Frog import Frog
+from organism.animal.carnivor.carnivors.Snake import Snake
+from organism.animal.carnivor.carnivors.Hawk import Hawk
+from organism.animal.carnivor.carnivors.SmallBird import SmallBird
+from organism.animal.herbivor.herbivors.Rabbit import Rabbit
+from organism.animal.herbivor.herbivors.GrassHopper import GrassHopper
+from organism.animal.herbivor.herbivors.Mouse import Mouse
+from maplogic.World import World
+from maplogic.Resource import Resource
+from maplogic.Grass import Grass
 
 
 class Simulation:
@@ -16,17 +23,47 @@ class Simulation:
     def __init__(self):
 
         pg.init()
-        self.screen = pg.display.set_mode((1920, 1080))
+        self.screen = pg.display.set_mode((1820, 980))
         self.clock = pg.time.Clock()
+
+        self.organism_list = []
+        self.world = World()
+
+        self.organism_population_over_time = []
+
+        self.debug_mode = True
 
     def spawn_organisms(self, organism_list: List[Any]) -> None:
         pass
 
-    def spawn_resources(self, resource_list: List[Any]) -> None:
-        pass
+    #Resource IDs: 1 = Grass, 2 = Water, 3 = Forest
+    def spawn_resources(self, resource_id: int, resource_radius: float, resource_position: List[Any], resource_type_id: int) -> None:
+        self.world.spawn_resource(resource_id, resource_radius, resource_position, resource_type_id)
 
     def update_all_Objects(self) -> None:
         pass
+
+    def draw_all_objects(self) -> None:
+
+        if self.debug_mode:
+            for resource in self.world.resource_map.values():
+                if resource.resource_type_id == 1:
+                    pg.draw.circle(self.screen, (58, 117, 33), resource.resource_position, resource.resource_radius)
+                elif resource.resource_type_id == 2:
+                    pg.draw.circle(self.screen, (0, 0, 255), resource.resource_position, resource.resource_radius)
+                elif resource.resource_type_id == 3:
+                    pg.draw.circle(self.screen, (150, 75, 0), resource.resource_position, resource.resource_radius)
+                else:
+                    print("Invalid Resource Type ID")
+
+    def test_one(self) -> None:
+        grass = 1
+        water = 2
+        forest = 3
+
+        self.spawn_resources(1, 100, [100, 100], grass)
+        self.spawn_resources(2, 100, [1720, 880], water)
+        self.spawn_resources(3, 100, [1720, 100], forest)
 
     def run_simulation(self) -> None:
         running = True
@@ -36,16 +73,18 @@ class Simulation:
                     running = False
 
 
-            self.screen.fill((0, 0, 0))
+            self.screen.fill((141, 240, 98))
+            self.draw_all_objects()
             pg.display.flip()
             self.clock.tick(30)
 
 
 if __name__ == "__main__":
     
-    # sim = Simulation()
-    # sim.run_simulation()
-    fox = Fox.Fox(1, True, 0.0, [0.0, 0.0], 1, 0.0, 100.0, 0.0, 100.0, 1.0, 1.0, 0.0, 100.0, 1.0, 1.0, 1.0, 1, 1.0, False, False, False, [1, 2, 3], [1, 2, 3], [1, 2, 3], 1)
+    sim = Simulation()
+    sim.test_one()
+    sim.run_simulation()
+
 
     
 
--- src/maplogic/World.py
+++ src/maplogic/World.py
@@ -1,11 +1,22 @@
+import sys
+sys.path.append("src/maplogic")
 from typing import List, Any
+from Resource import Resource
 
 class World:
 
-    def __init__(self, resource_list: List[Any], time_of_day: int):
-        self.resource_list = resource_list
-        self.time_of_day = time_of_day
+    def __init__(self):
+        self.resource_map = {}
+        self.time_of_day = 0
 
+    #TODO: Implement this method
+    def update_time_of_day(self) -> None:
+        pass
+
+    def spawn_resource(self, resource_id: int, resource_radius: float, resource_position: List[Any], resource_type_id: int) -> None:
+        resource = Resource(resource_id, resource_position, resource_radius, resource_type_id)
+        self.resource_map[resource_id] = resource
+        
 
 
 
